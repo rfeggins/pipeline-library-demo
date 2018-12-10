6806b92c51f72bf11538a7f65440a94971edfd30
rfeggins <rfeggins@us.ibm.com>
2018-12-10 15:44:52 -0500

    added Check Jira content to shared library

---
+++ vars/checkJiraIssue.groovy
@@ -0,0 +1,29 @@
+#!/usr/bin/env groovy
+
+def call( gitCommit ) {
+  echo "Executing checkJiraIssue"
+  echo "gitCommit" + "${gitCommit}"
+  echo "GIT_COMMIT" + "${GIT_COMMIT}"
+
+  def gitCommitMsg = sh(returnStdout: true, script:  'git log --oneline -1 ${GIT_COMMIT}')
+
+  def jiraIDs = sh(returnStdout: true, script: 'git log --oneline -1 ${GIT_COMMIT} | grep -i -o ddtl-.... | tr -d "\n" ')
+
+  echo "Jenkins pipeline shell output"
+  echo "git commit message  " + gitCommitMsg
+  echo "####" + jiraIDs + "####"
+  ddtl_ID = jiraIDs
+
+  // validate that ddtl number is found within Git gitCommitMsg message
+  if (ddtl_ID == "") {
+    echo "##########################"
+    echo "Jira Process Error 101 - No DDTL Issue found in Git Commit Message\n for build  "
+    echo "commit message::     " + gitCommitMsg
+    echo "##########################"
+    currentBuild.result = 'FAILURE'
+
+  } else {
+    echo "Following DDTL IDs retrieved from git commit message"
+    echo ddtl_ID
+  }
+}
